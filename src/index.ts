f3a0113a2524236ee38c28592a625da17692c496
@FranCarn <franco@bowerystudio.co>
2023-02-12 21:18:26 -0300

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,27 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuidV4 } from 'uuid';
 
-import confetti from 'canvas-confetti';
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+};
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const list = document.getElementById('list') as HTMLUListElement;
+const form = document.getElementById('new-task-form') as HTMLFormElement;
+const input = document.getElementById('new-task-title') as HTMLInputElement;
+
+form?.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (!input.value) return;
+
+  const newTask = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  };
+  addListItem(newTask);
+});
+
+const addListItem = (task: Task) => {};
